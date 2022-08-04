6d798be4810aeba64db6d099535f995f1cd3421f
unknown <cassioalexsander@gmail.com>
2022-08-03 22:13:04 -0300

    Criado o loop e as opções

--- Work1_CassioCorrea/Program.cs
+++ Work1_CassioCorrea/Program.cs
@@ -18,33 +18,49 @@ namespace Work1_CassioCorrea
             
             Entity account = new Entity("account");
             Console.Write("Digite o nome da Conta: ");
-            String name = Console.ReadLine();
-            account["name"] = name;
+            String accountName = Console.ReadLine();
+            account["name"] = accountName;
 
             Console.Write("Digite o telefone: ");
-            String phone = Console.ReadLine();
-            account["telephone1"] = phone;
+            String accountPhone = Console.ReadLine();
+            account["telephone1"] = accountPhone;
 
             Console.Write("Digite o Website: ");
-            String site = Console.ReadLine();
-            account["websiteurl"] = site;
-
-            Console.Write("Deseja criar um contato para essa conta? (S/N)");
-            String choice = Console.ReadLine();
-            if(choice.ToUpper() == "S")
+            String accountSite = Console.ReadLine();
+            account["websiteurl"] = accountSite;
+            
+            bool error = false;
+            do
             {
-                Guid accountId = service.Create(account);
+                Console.Write("Deseja criar um contato para essa conta? (S/N)");
+                String choice = Console.ReadLine();
+                if (choice.ToUpper() == "S")
+                    {
+                        Guid accountId = service.Create(account);
 
-                Entity contact = new Entity("contact");
-                contact["firstname"] = "Natalia";
-                contact["lastname"] = "Branisci";
-                contact["jobtitle"] = "Illustrator";
-                contact["parentcustomerid"] = new EntityReference("account", accountId);
-                service.Create(contact);
-            }else if (choice.ToUpper() == "N")
-            {
-                Console.WriteLine("Encerrando o programa. Obrigado!");
-            }
+                        Entity contact = new Entity("contact");
+                        Console.Write("Digite o primeiro nome: ");
+                        String contactName = Console.ReadLine();
+                        contact["firstname"] = contactName;
+                        Console.Write("Digite o sobrenome: ");
+                        String contactSurname = Console.ReadLine();
+                        contact["lastname"] = contactSurname;
+                        Console.Write("Digite o cargo: ");
+                        String contactJobTitle = Console.ReadLine();
+                        contact["jobtitle"] = contactJobTitle;
+                        contact["parentcustomerid"] = new EntityReference("account", accountId);
+                        service.Create(contact);
+                    }
+                else if (choice.ToUpper() == "N")
+                    {
+                        Console.WriteLine("Encerrando o programa. Obrigado!");
+                    }
+                else
+                    {
+                        error = true;
+                        Console.WriteLine("Opção inválida!");
+                    }
+            }while (error);
             
 
         }
