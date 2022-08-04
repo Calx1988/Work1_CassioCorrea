0bf62be7a73ef9b7f6363e7362f15bd8d13a88d7
unknown <cassioalexsander@gmail.com>
2022-08-04 02:23:33 -0300

    Alterado para classes Account e Contact

---
+++ Work1_CassioCorrea/Contact.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1_CassioCorrea
+{
+    internal class Contact
+    {
+        String firstName;
+        String lastName;
+        String email;
+        String jobTitle;
+
+        public Contact()
+        {
+        }
+
+        public Entity createContact(Guid id)
+        {
+            Entity contact = new Entity("contact");
+            Console.Write("Digite o primeiro nome: ");
+            firstName = Console.ReadLine();
+            contact["firstname"] = firstName;
+            Console.Write("Digite o sobrenome: ");
+            lastName = Console.ReadLine();
+            contact["lastname"] = lastName;
+            Console.Write("Digite o cargo: ");
+            jobTitle = Console.ReadLine();
+            contact["jobtitle"] = jobTitle;
+            contact["parentcustomerid"] = new EntityReference("account", id);
+            return contact;
+        }
+
+    }
+}
--- Work1_CassioCorrea/Program.cs
+++ Work1_CassioCorrea/Program.cs
@@ -15,50 +15,31 @@ namespace Work1_CassioCorrea
             MyConnector connector = new MyConnector();
             IOrganizationService service = connector.createConnection();
             
-            
-            Entity account = new Entity("account");
-            Console.Write("Digite o nome da Conta: ");
-            String accountName = Console.ReadLine();
-            account["name"] = accountName;
+            Account account = new Account();         
 
-            Console.Write("Digite o telefone: ");
-            String accountPhone = Console.ReadLine();
-            account["telephone1"] = accountPhone;
+            Guid accountId = service.Create(account.createAccount());
 
-            Console.Write("Digite o Website: ");
-            String accountSite = Console.ReadLine();
-            account["websiteurl"] = accountSite;
-            
+            Console.WriteLine(account.toString());
+
             bool error = false;
             do
             {
-                Console.Write("Deseja criar um contato para essa conta? (S/N)");
+                error = false;
+                Console.Write("\nDeseja criar um contato para essa conta? (S/N)");
                 String choice = Console.ReadLine();
                 if (choice.ToUpper() == "S")
                     {
-                        Guid accountId = service.Create(account);
-
-                        Entity contact = new Entity("contact");
-                        Console.Write("Digite o primeiro nome: ");
-                        String contactName = Console.ReadLine();
-                        contact["firstname"] = contactName;
-                        Console.Write("Digite o sobrenome: ");
-                        String contactSurname = Console.ReadLine();
-                        contact["lastname"] = contactSurname;
-                        Console.Write("Digite o cargo: ");
-                        String contactJobTitle = Console.ReadLine();
-                        contact["jobtitle"] = contactJobTitle;
-                        contact["parentcustomerid"] = new EntityReference("account", accountId);
-                        service.Create(contact);
+                        Contact contact = new Contact();                        
+                        service.Create(contact.createContact(accountId));
                     }
                 else if (choice.ToUpper() == "N")
                     {
-                        Console.WriteLine("Encerrando o programa. Obrigado!");
+                        Console.WriteLine("\nEncerrando o programa. Obrigado!");
                     }
                 else
                     {
                         error = true;
-                        Console.WriteLine("Opção inválida!");
+                        Console.WriteLine("\nOpção inválida!");
                     }
             }while (error);
             
